ce5fd53e219867449b6dc2ab353fa11369d7e644
Muhamad Ivan <muhamadivann25@gmail.com>
2022-09-15 17:16:58 +0700

    update contact pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,7 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 
-import styles from 'src/styles/Home.module.css'
+// import styles from 'src/styles/Home.module.css'
 
 export default function Contact() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Zixta Logistics Services</title>
-                <meta name="description" content="Zixta Logistics Services" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        </div>
-    )
+    return null
 }
